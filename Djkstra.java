fd59c0734b12105e9473f89a3e8583926d22724e
ErikThorsson <erikorndahl@24-72-248-126.mduconnect.com>
2016-03-23 23:38:15 -0400

    cleaned up code

--- Djkstra.java
+++ Djkstra.java
@@ -9,26 +9,26 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Djkstra {
-	
+
 	public static void main(String [] args) throws Exception {
 
 		//pass an array of arrays from the parse...
 		Parse p = new Parse();
 		Vertex[][] nodes = p.computeEdges();
-				
+
 		int n = p.getN();
 		int[] dis = new int[n];
 		int[] prev =  new int[n];
 		Hashtable<Integer, Vertex> h = new Hashtable<Integer, Vertex>();
-		
+
 		//will need to identify depth of tree
 		int depth = (int) Math.ceil((Math.log(n) / Math.log(2)));
 		BinaryHeap b = new BinaryHeap((int)Math.pow(2, depth + 1)); // pad lowest level with nulls
-				
+
 		//set up source vertex
 		dis[0] = 0;
 		b.insert(new Vertex(0, 0));
-		
+
 		//set distance from source to all vertices as practically infinite
 		for(int i=1; i<n; i++){
 			dis[i] = 100000;
@@ -36,33 +36,33 @@ public class Djkstra {
 			b.insert(v);
 			h.put(i, v); //put each vertex in a hash table by its vertex number
 		}
-				
+
 		while(!b.isEmpty()) {
 			Vertex u = b.extractMin();
-			
+
 			//now get neighbors of u
 			for(int i = 0; i< nodes[u.getV()].length; i++){
 				if(nodes[u.getV()][i] != null) {
 
-				//get the distance from source to the neighbor of u
-				int alt = nodes[u.getV()][i].getD() + dis[u.getV()];
-				
-				//if the alt route is less than this vertex's original route from source to itself, replace its smallest path
-				if( alt < dis[nodes[u.getV()][i].getV()]) {
-					//ugly but gets the index in the heap of the vertex of the neighbor. Allows for an O(1) heap update
-					b.update(((Vertex) h.get(nodes[u.getV()][i].getV())).getIndex(), alt); //update the path value in the heap
-					dis[nodes[u.getV()][i].getV()] = alt; //
-					prev[nodes[u.getV()][i].getV()] = u.getV();
+					//get the distance from source to the neighbor of u
+					int alt = nodes[u.getV()][i].getD() + dis[u.getV()];
+
+					//if the alt route is less than this vertex's original route from source to itself, replace its smallest path
+					if( alt < dis[nodes[u.getV()][i].getV()]) {
+						//ugly but gets the index in the heap of the vertex of the neighbor. Allows for an O(1) heap update
+						b.update(((Vertex) h.get(nodes[u.getV()][i].getV())).getIndex(), alt); //update the path value in the heap
+						dis[nodes[u.getV()][i].getV()] = alt; //
+						prev[nodes[u.getV()][i].getV()] = u.getV();
+					}
 				}
 			}
-			}
 		}
-		
+
 		int sum = 0;
 		for(int i = 0; i< dis.length; i++) {
-				sum += dis[i];
+			sum += dis[i];
 		}
-		
+
 		System.out.println(sum);
 
 	}
--- Parse.java
+++ Parse.java
@@ -7,72 +7,72 @@ import java.io.IOException;
 public class Parse {
 	private int n = 0;
 	private int m = 0;
-	
+
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Parse p = new Parse();
 		Vertex[][] v = p.computeEdges();
 		//Vertex[][] v = p.parse();
 		System.out.print(v[999][0].getV());
 	}
-	
+
 	public Vertex[][] parse() throws FileNotFoundException, IOException{
 		int v = 0,count = 0;
 		Vertex[][] vertices = null;
-	
-		try(BufferedReader br = new BufferedReader(new FileReader("/Users/erikorndahl/Desktop/Algo/25000.txt"))) {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
-	    	boolean inside = false;
 
-		    while (line != null) {
-		    	
-		    	//get n and m
-		    	if(line.contains("n")) {
-		    		String split[] = line.split(" ");
-		    		String splitN[] = split[0].split("=");
-		    		n = Integer.parseInt(splitN[1]);
-		    		String splitM[] = split[1].split("=");
-		    		m = Integer.parseInt(splitM[1]);
-		    		//System.out.print(n + " " + m);
-		    		vertices = new Vertex[n][];
-		    		for(int i = 0; i < n; i++) {
-		    			vertices[i] = new Vertex[200];
-		    		}
-		    	}
-		    	
-		    	//if line contains 1 number and no spaces...
-		    	if(line.matches("^[0-9]+")) {
-		    		v = Integer.parseInt(line.trim());
-		    		inside = true;
-		    		count = 0;
-		    		//System.out.println(line);
-			        line = br.readLine();
-		    		continue;
-		    	}
-		    	
-		    	if(line.trim().isEmpty()) { 
-		    		inside = false;
-		    		//System.out.println("BLANK");
-		    	}
-		    	
-		    	if(inside == true) {
-		    		//System.out.println(line);
-		    		String[] split = line.split("\\s+");
-		    		int neigh = Integer.parseInt(split[1].trim());
-		    		//System.out.println(neigh);
-		    		int dis = Integer.parseInt(split[2].trim());
-		    		vertices[v][count] = new Vertex(neigh, dis);
-		    		count++;
-		    	}
-		    			    		
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
+		try(BufferedReader br = new BufferedReader(new FileReader("/Users/erikorndahl/Desktop/Algo/25000.txt"))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			boolean inside = false;
+
+			while (line != null) {
+
+				//get n and m
+				if(line.contains("n")) {
+					String split[] = line.split(" ");
+					String splitN[] = split[0].split("=");
+					n = Integer.parseInt(splitN[1]);
+					String splitM[] = split[1].split("=");
+					m = Integer.parseInt(splitM[1]);
+					//System.out.print(n + " " + m);
+					vertices = new Vertex[n][];
+					for(int i = 0; i < n; i++) {
+						vertices[i] = new Vertex[200];
+					}
+				}
+
+				//if line contains 1 number and no spaces...
+				if(line.matches("^[0-9]+")) {
+					v = Integer.parseInt(line.trim());
+					inside = true;
+					count = 0;
+					//System.out.println(line);
+					line = br.readLine();
+					continue;
+				}
+
+				if(line.trim().isEmpty()) { 
+					inside = false;
+					//System.out.println("BLANK");
+				}
+
+				if(inside == true) {
+					//System.out.println(line);
+					String[] split = line.split("\\s+");
+					int neigh = Integer.parseInt(split[1].trim());
+					//System.out.println(neigh);
+					int dis = Integer.parseInt(split[2].trim());
+					vertices[v][count] = new Vertex(neigh, dis);
+					count++;
+				}
+
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
 		}
 		return vertices;
 	}
-	
+
 	public Vertex[][] computeEdges() throws FileNotFoundException, IOException{
 		Vertex[][] v = parse();
 		for(int i = 0; i< v.length; i++) {
@@ -86,26 +86,26 @@ public class Parse {
 							v[v[i][k].getV()][0] = new Vertex(i,v[i][k].getD());
 						}
 						if(v[v[i][k].getV()][j] != null) {
-					if(v[v[i][k].getV()][j].getV() == v[i][k].getV())
-						found = true;
-					if(found == false && v[v[i][k].getV()][j + 1] == null) {
-						//find empty index inside and insert edge
-						for(int l = 0; l < v[i].length; l++) {
-							if(v[v[i][k].getV()][l] == null) {
-								v[v[i][k].getV()][l] = new Vertex(i,v[i][k].getD());
+							if(v[v[i][k].getV()][j].getV() == v[i][k].getV())
+								found = true;
+							if(found == false && v[v[i][k].getV()][j + 1] == null) {
+								//find empty index inside and insert edge
+								for(int l = 0; l < v[i].length; l++) {
+									if(v[v[i][k].getV()][l] == null) {
+										v[v[i][k].getV()][l] = new Vertex(i,v[i][k].getD());
+										break;
+									}
+								}
 								break;
-							}
+							}	
 						}
-						break;
-					}	
-				}
 					}
 				}
 			}
 		}
 		return v;
 	}
-	
+
 	public int getN(){return n;}
 
 	public int getM(){return m;}
